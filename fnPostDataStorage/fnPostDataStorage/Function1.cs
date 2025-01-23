a3fdb3e857261ae178be6dbe2e657718fee4ff45
Fernando Matheus <fernando.matheuss@hotmail.com>
2025-01-23 00:24:51 -0300

    Aumentando limete de storage

--- fnPostDataStorage/fnPostDataStorage/Function1.cs
+++ fnPostDataStorage/fnPostDataStorage/Function1.cs
@@ -1,7 +1,11 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace fnPostDataStorage
 {
@@ -15,10 +19,46 @@ namespace fnPostDataStorage
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            if (!req.Headers.TryGetValue("file-type", out var fileTypeHeader))
+            {
+                return new BadRequestObjectResult("O cabeçalho 'file-type' é obrigatório.");
+            }
+
+            var fileType = fileTypeHeader.ToString();
+            var form = await req.ReadFormAsync();
+            var file = form.Files["file"];
+
+            if (file == null || file.Length == 0)
+            {
+                return new BadRequestObjectResult("O arquivo não foi enviado.");
+            }
+
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = fileType;
+            BlobClient blobClient = new BlobClient(connectionString, containerName, file.FileName);
+            BlobContainerClient containerClient = new BlobContainerClient(connectionString, containerName);
+
+            await containerClient.CreateIfNotExistsAsync();
+            await containerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
+
+            string blobName = file.FileName;
+            var blob = containerClient.GetBlobClient(blobName);
+
+            using (var stream = file.OpenReadStream())
+            {
+                await blob.UploadAsync(stream, true);
+            }
+
+            return new OkObjectResult(
+                new
+                {
+                    Message = "Arquivo armazenado com sucesso.",
+                    BlobUri = blob.Uri
+                });
         }
     }
 }
